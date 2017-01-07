0d5f0f48d4ba2f1d427da77c32c20b863ff1ab8d
Misigno <efournier92@gmail.com>
2017-01-07 12:08:35 -0500

    add equal vs strictlyEqual functions

--- 03_AsyncCallbacks/app.js
+++ 03_AsyncCallbacks/app.js
@@ -1,12 +1,12 @@
 // long running function
 function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms){}
-    console.log('finished function');
+  var ms = 3000 + new Date().getTime();
+  while (new Date() < ms){}
+  console.log('finished function');
 }
 
 function clickHandler() {
-    console.log('click event!');   
+  console.log('click event!');   
 }
 
 // listen for the click event
@@ -23,3 +23,22 @@ a = b = c;
 console.log("a: ", a);
 console.log("b: ", b);
 console.log("c: ", c);
+
+var a = 0;
+var b = false;
+
+function equal() {
+  if (a == b) {
+    console.log('They are equal.');
+  } else {
+    console.log('Nope! Not equal.');
+  }
+}
+
+function strictlyEqual() {
+  if (a == b) {
+    console.log('They are strictly equal.');
+  } else {
+    console.log('Nope! Not strictly equal.');
+  }
+}
