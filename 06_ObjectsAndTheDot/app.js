1e04c62aae3f2856d7cac170312acbad2f07dec2
Misigno <efournier92@gmail.com>
2017-01-10 19:30:00 -0500

    add dot operator logic

--- 06_ObjectsAndTheDot/app.js
+++ 06_ObjectsAndTheDot/app.js
@@ -1,5 +1,8 @@
 var person = new Object();
 
 person["firstname"] = "Eric";
+person.lastname = "Fournier";
 
-console.log(person.firstname);
+console.log(person.firstname, person["lastname"]);
+console.log(person);
+
