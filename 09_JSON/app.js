fd84f95240942d9659634cb971da7e43e7d8d3d6
Misigno <efournier92@gmail.com>
2017-01-10 19:33:42 -0500

    add JSON syntax sample to JSON project

---
+++ 09_JSON/app.js
@@ -0,0 +1,12 @@
+var objectLiteral = {
+  firstName: 'Mary',
+  isAProgrammer: true
+}
+
+console.log(objectLiteral); 
+
+// SAMPLE JSON SYNTAX
+// {
+//   "firstName": "Mary",
+//   "isAProgrammer": true
+// }
