d6c7d1ef58ad320a4622026cd671da8e60416ea9
Misigno <efournier92@gmail.com>
2017-01-10 19:31:57 -0500

    add greet function to ObjectLiterals

--- 07_ObjectLiterals/app.js
+++ 07_ObjectLiterals/app.js
@@ -1,3 +1,17 @@
-var person = { firstname: 'Eric', lastname: 'Fournier'};
+var person = {
+  firstName: 'Eric',
+  lastName: 'Fournier',
+  address: {
+    street: '123 Fake St.',
+    city: 'New York',
+    state: 'NY'
+  }
+};
+
+function greet(person) {
+  console.log('Hi', person.firstName);
+};
+
+greet(person);
 
 console.log(person);
