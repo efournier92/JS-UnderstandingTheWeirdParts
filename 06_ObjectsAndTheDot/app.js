2f673e27657742364e51cf31a2b41abfe844b90a
Misigno <efournier92@gmail.com>
2017-01-10 19:30:18 -0500

    add address object to person

--- 06_ObjectsAndTheDot/app.js
+++ 06_ObjectsAndTheDot/app.js
@@ -2,7 +2,10 @@ var person = new Object();
 
 person["firstname"] = "Eric";
 person.lastname = "Fournier";
-
 console.log(person.firstname, person["lastname"]);
-console.log(person);
 
+person.address = new Object();
+person.address.street = "123 Fake St.";
+console.log(person.address.street);
+
+console.log(person);
