00b8438b4ff953e9f731ec81ebf12d6200522829
Misigno <efournier92@gmail.com>
2017-01-12 21:05:09 -0500

    write obj mutate function

--- 12_ByValueByReference/app.js
+++ 12_ByValueByReference/app.js
@@ -9,3 +9,21 @@ a = 2;
 console.log('a:', a);
 console.log('b:', b);
 
+// by reference (all objects, including functions)
+var c = { greeting: 'hi' };
+var d;
+
+d = c;
+
+c.greeting = 'hello'; // mutate
+
+console.log('c1:', c);
+console.log('d1:', d);
+
+function changeGreeting(obj) {
+  obj.greeting = 'Hola'; // mutate
+}
+
+changeGreeting(d);
+console.log('c2:', c);
+console.log('d2:', d);
