49eb2f21630b4e000f83f1f15bb54f8bc3957c3a
Misigno <efournier92@gmail.com>
2017-01-12 21:01:54 -0500

    add FunctionsAreObjects project

--- 09_JSON/app.js
+++ 09_JSON/app.js
@@ -2,11 +2,9 @@ var objectLiteral = {
   firstName: 'Mari',
   isAProgrammer: false 
 }
-
 console.log('Stringified:', JSON.stringify(objectLiteral));
 
 var jsonValue = JSON.parse('{ "firstName": "Tony", "isAProgrammer": true }');
-
 console.log('Parsed:', jsonValue);
 
 // SAMPLE JSON SYNTAX
