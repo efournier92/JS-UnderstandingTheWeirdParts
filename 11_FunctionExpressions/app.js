a4834baed6043d1a564c3f9bf419928c3155ac27
Misigno <efournier92@gmail.com>
2017-01-12 21:03:28 -0500

    pass anonymous function

--- 11_FunctionExpressions/app.js
+++ 11_FunctionExpressions/app.js
@@ -1,9 +1,23 @@
-greet();
-
-function greet() {
-  console.log('hi');
-}
+(function greet() {
+  console.log('hello');
+})();
 
 var anonymousGreet = function() {
-  console.log('hi');
+  console.log('anonymous: hi');
 }
+
+anonymousGreet();
+
+function log(a) {
+  a();
+}
+
+log(3);
+
+log("Test String");
+
+log(function() {
+  console.log('anonymous function: Hi!')
+});
+
+
