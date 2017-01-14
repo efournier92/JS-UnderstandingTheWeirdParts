39a6483015db6ca8a5f0f670c611f3a9b73406a0
Misigno <efournier92@gmail.com>
2017-01-14 12:23:43 -0500

    write null argument passing example

---
+++ 15_Arguments/app.js
@@ -0,0 +1,11 @@
+function greet(firstName, lastName, language) {
+  console.log(firstName);
+  console.log(lastName);
+  console.log(language);
+  console.log('_________');
+}
+
+greet();
+greet('Ringo');
+greet('Ringo', 'Starr');
+greet('Ringo', 'Starr', 'English');
