34796410d34f12d182aba7d7cdea03721fd9ab7a
Misigno <efournier92@gmail.com>
2017-01-14 12:21:16 -0500

    add this nested scoping sample

--- 13_ObjectFunctionsAndThis/app.js
+++ 13_ObjectFunctionsAndThis/app.js
@@ -1,5 +1,28 @@
 function a() {
   console.log(this);
+  this.newVariable = 'Why, Hello';
+}
+
+var b = function() {
+  console.log(this);
 }
 
 a();
+console.log(newVariable);
+b();
+
+var c = {
+  name: 'The c object',
+  log: function() {
+    this.name = 'updated c object';
+    console.log(this);
+
+    var setName = function(newName) {
+      this.name = newName;
+    }
+    setname('Updated c object AGAIN!');
+    console.log(this);
+  }
+}
+
+c.log();
