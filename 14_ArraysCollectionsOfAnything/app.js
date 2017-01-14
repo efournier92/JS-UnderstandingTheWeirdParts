733bf361f4bbee3155fe4cf748868fc2a3a6656e
Misigno <efournier92@gmail.com>
2017-01-14 12:22:22 -0500

    add multi-type array example

---
+++ 14_ArraysCollectionsOfAnything/app.js
@@ -0,0 +1,16 @@
+var arr = [
+  1,
+  false,
+  3,
+  {
+    name: 'Eric',
+    address: '123 Fake St.'
+  },
+  function(name) {
+    var greeting = 'Hello'; 
+    console.log(greeting + name);
+  },
+  "Hello"
+];
+
+console.log(arr);
