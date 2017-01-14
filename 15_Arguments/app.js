939571de3bad808facab12c7fc28dd27fb5890a2
Misigno <efournier92@gmail.com>
2017-01-14 12:24:27 -0500

    finish ArraysCollectionsOfAnything project

--- 15_Arguments/app.js
+++ 15_Arguments/app.js
@@ -2,10 +2,17 @@ function greet(firstName, lastName, language) {
 
   language = language || 'Spanish';
 
+  if (arguments.length === 0) {
+    console.log('Missing Paramaters!');
+    console.log('_________');
+    return;
+  }
+
   console.log(firstName);
   console.log(lastName);
   console.log(language);
   console.log(arguments);
+  console.log('1st Argument:', arguments[0]);
   console.log('_________');
 }
 
