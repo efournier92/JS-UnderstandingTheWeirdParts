70d1e773b54b610188f4f37a722d97539a5b6ccc
Misigno <efournier92@gmail.com>
2017-01-15 11:07:44 -0500

    finish IIFEs project

--- 18_IIFEs/app.js
+++ 18_IIFEs/app.js
@@ -1,19 +1,24 @@
-// regular function declaration
+// function statement 
 function greet(name) {
   console.log('Hello', name);
 }
 greet('Eric');
 
-// using a function expression 
+// function expression 
 var greetFunc = function(name) {
   console.log('Hello', name);
 };
 greetFunc('Ringo');
 
-// using an Immediately Invoked Function Expression (IIFE)
+// IIFE (Immediately Invoked Function Expression)
 var greeting = function(name) {
-
   console.log('Hello', name);
-
 }('George');
+
+// another IIFE
+var firstName = 'Paul';
+(function(name) {
+  var greeting = 'Hello';
+  console.log('inside IIFE:', greeting, name);
+}(firstName));
 
