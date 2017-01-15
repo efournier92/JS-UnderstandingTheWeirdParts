ef32687a13f674ebd0b98e58899f10c25f392caa
Misigno <efournier92@gmail.com>
2017-01-15 11:06:14 -0500

    finish Whitespace project

---
+++ 17_Whitespace/app.js
@@ -0,0 +1,21 @@
+var 
+  // first name of person 
+  firstName,
+
+  // last name of person 
+  lastName,
+
+  // desired language 
+  // can be 'English' or 'Spanish'
+  language;
+
+var person = {
+  // the first name
+  firstName: 'Ringo',
+
+  // the last name
+  // (always required)
+  lastName: 'Starr'
+}
+
+console.log(person);
