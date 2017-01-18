1d29e6fd02689a6264cbed71a1705ebf43fe2407
Misigno <efournier92@gmail.com>
2017-01-18 15:54:33 -0500

    bind person & logName

--- 23_CallApplyBind/app.js
+++ 23_CallApplyBind/app.js
@@ -8,3 +8,13 @@ var person = {
 
   }
 }
+
+var logName = function(lang1, lang2) {
+
+  console.log('Logged: ' + this.getFullName());
+
+}
+
+var logPersonName = logName.bind(person);
+
+logPersonName();
