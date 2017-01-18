a153aea0cf8103b0796976bebe5a91755cf2e81b
Misigno <efournier92@gmail.com>
2017-01-18 15:53:02 -0500

    finish ClosuresAndCallbacks

--- 22_ClosuresAndCallbacks/app.js
+++ 22_ClosuresAndCallbacks/app.js
@@ -5,6 +5,31 @@ function sayHiLater() {
   setTimeout(function() {
     console.log(greeting) 
   }, 3000);
+
 }
 
 sayHiLater();
+
+// //jQuery uses function expressions & first-class functions
+// $("button").click(function() {
+//
+// });
+
+function tellMeWhenDone(callback) {
+  var a = 1000;
+  var b = 2000;
+
+  callback(); // runs the functions it's given
+}
+
+tellMeWhenDone(function() {
+  console.log("I'm done!")
+})
+
+tellMeWhenDone(function() {
+  console.log("I'm done too!")
+})
+
+tellMeWhenDone(function() {
+  console.log("I'm done three!")
+})
