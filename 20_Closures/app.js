999690c877e8aa27d215efb86e5aa3caa78c44da
Misigno <efournier92@gmail.com>
2017-01-17 19:41:24 -0500

    finish CLosures project

--- 20_Closures/app.js
+++ 20_Closures/app.js
@@ -32,3 +32,28 @@ fs[0]();
 fs[1]();
 fs[2]();
 
+// this function will return 1, 2, 3 as array elements
+function buildFunctions2() {
+
+  var arr = [];
+
+  for (var i = 0; i < 3; i++) {
+    arr.push(
+      (function(j) {
+        return function() {
+          console.log(j);
+        }
+      }(i))
+    )
+  }
+
+  return arr;
+
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
+
