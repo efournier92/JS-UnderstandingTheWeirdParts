a99ecc1fe27a14b5358170e23c61e7ca832ef129
Misigno <efournier92@gmail.com>
2017-01-17 19:40:40 -0500

    write closure array function error example

--- 20_Closures/app.js
+++ 20_Closures/app.js
@@ -10,3 +10,25 @@ greet('Hello')('Eric');
 
 var sayHi = greet('Hi');
 sayHi('Ringo');
+
+// this function will return 3 for all array elements
+function buildFunctions() {
+
+  var arr = [];
+
+  for (var i = 0; i < 3; i++) {
+    arr.push(function() {
+      console.log(i);
+    });
+  }
+
+  return arr;
+
+}
+
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
+
