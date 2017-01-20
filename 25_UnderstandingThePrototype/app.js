89f45602835bc59cbe0c0d7f9483ccfdf1bf7ba3
Misigno <efournier92@gmail.com>
2017-01-19 19:46:06 -0500

    code dangerous proto example

--- 25_UnderstandingThePrototype/app.js
+++ 25_UnderstandingThePrototype/app.js
@@ -1,1 +1,20 @@
+var person = {
+  firstName: 'Default',
+  lastName: 'Default',
+  getFullName: function() {
+    return this.firstName + ' ' + this.lastName;
+  }
+}
+
+var ringo = {
+  firstName: 'Ringo',
+  lastName: 'Starr'
+}
+
+// modern browsers allow this, but it's slow
+// DON'T DO THIS
+ringo.__proto__ = person;
+console.log(ringo.getFullName());
+console.log(ringo.firstName);
+console.log(ringo.lastName);
 
