2ae8c8ae65d8bdeece9899e48c3b1711a67a444a
Misigno <efournier92@gmail.com>
2017-01-20 21:11:16 -0500

    add george object

--- 27_FunctionConstructorsNewHistory/app.js
+++ 27_FunctionConstructorsNewHistory/app.js
@@ -1,9 +1,14 @@
-function Person() {
+function Person(firstName, lastName) {
 
-  this.firstName = 'Ringo';
-  this.lastName = 'Starr';
+  console.log(this)
+  this.firstName = firstName;
+  this.lastName = lastName;
+  console.log('function invoked!')
 
 }
 
-var ringo = new Person();
+var ringo = new Person('Ringo', 'Starr');
 console.log(ringo);
+
+var george = new Person('George', 'Harrison');
+console.log(george);
