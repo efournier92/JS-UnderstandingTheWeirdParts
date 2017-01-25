a711ecd70f89b7da1a60105d6bb0368007d16c67
Misigno <efournier92@gmail.com>
2017-01-24 21:09:22 -0500

    add 2 pure prototypal inheritance functions

--- 30_ObjectCreateAndPrototypal/app.js
+++ 30_ObjectCreateAndPrototypal/app.js
@@ -4,12 +4,30 @@ var arr = ['John', 'Paul', 'George', 'Ringo'];
 for (var prop in arr) {
   console.log(prop + ': ' + arr[prop]);
 }
-
 console.log('_________');
 
 // Safe 
 for (var i = 0; i < arr.length; i++) {
   console.log(i + ': ' + arr[i]);
 }
-
 console.log('_________');
+
+var person = {
+  firstName: 'Default',
+  lastName: 'Default',
+  greet: function() {
+    return 'Hi ' + this.firstName;
+  }
+}
+
+var ringo = Object.create(person);
+console.log(ringo);
+console.log(ringo.greet());
+console.log('_________');
+
+ringo.firstName = 'Ringo';
+ringo.lastName = 'Starr';
+console.log(ringo);
+console.log(ringo.greet());
+console.log('_________');
+
