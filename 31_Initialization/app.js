d9ee5c6184bb0fba0446ca02ab208c20d7acf3ec
Misigno <efournier92@gmail.com>
2017-01-24 21:10:24 -0500

    initialize multi-object 'people'

---
+++ 31_Initialization/app.js
@@ -0,0 +1,23 @@
+var people = [
+  {
+    // the 'ringo' object
+    firstName: 'Ringo',
+    lastName: 'Starr',
+    addressed: [
+      '123 Yellow Submarine Blvd',
+      '485 Abbey Rd'
+    ]
+  },
+  {
+    // the 'john' object
+    firstName: 'John',
+    lastName: 'Lennon',
+    addressed: [
+      '485 Abbey Rd',
+      '112 Penny Ln'
+    ],
+    greet: function() {
+      return 'Hello!';
+    }
+  }
+]
