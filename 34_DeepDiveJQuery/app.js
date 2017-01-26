c19892361ebb38f2562e417d785a8a6d025c6659
Misigno <efournier92@gmail.com>
2017-01-25 19:49:58 -0500

    write add class & remove class examples

--- 34_DeepDiveJQuery/app.js
+++ 34_DeepDiveJQuery/app.js
@@ -3,3 +3,6 @@ console.log(peopleLines);
 
 var taskLines = $("ul.tasks li");
 console.log(taskLines);
+
+var newPeople = $("ul.people").addClass("newclass").removeClass("people");
+console.log(peopleLines);
