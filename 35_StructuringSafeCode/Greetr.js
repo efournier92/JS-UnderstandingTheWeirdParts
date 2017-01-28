d8319007548e91669f919b037fcc535c8fdbb743
Misigno <efournier92@gmail.com>
2017-01-28 14:05:33 -0500

    add project requirement comments

---
+++ 35_StructuringSafeCode/Greetr.js
@@ -0,0 +1,9 @@
+// When give a first name, last name, and optional language, it generates formal & informal greetings
+//
+// Supports bothn English & Spanish languages
+//
+// Reusable library / framework
+//
+// Simple to type 'G$()' structure
+//
+// Supports jQuery
