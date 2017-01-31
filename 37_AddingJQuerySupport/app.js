d5bdf024449d6ba477b5df6c02a6a1c1fa69b485
Misigno <efournier92@gmail.com>
2017-01-31 18:22:20 -0500

    add comments to library implementation

--- 37_AddingJQuerySupport/app.js
+++ 37_AddingJQuerySupport/app.js
@@ -1,13 +1,20 @@
+// gets a new object
 var g = G$('Ringo', 'Starr');
 
+// use our chainable methods
 g.greet().setLang('en').greet(true);
 
 $('#login').click(function() {
   
+  // create new 'Greetr' object
   var loginGreetr = G$('Ringo', 'Starr');
 
+  // hide the login on screen
   $('#logindiv').hide();
 
+  // fire HTML greeting
+  // pass '#greeting' as the selector
+  // also log the welcome
   loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
 
 })
